834ccdfa865aca2fefe2228ea7b4f5199d91ce5a
root <root@dps-ubuntu-cfcmaster.rtp.raleigh.ibm.com>
2017-08-02 11:03:29 -0400

    Added axios to new node install

--- app/src/App.js
+++ app/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+//import components
+import Pantry from './components/Pantry.js';
 
 class App extends Component {
   render() {
@@ -13,6 +15,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+	<Pantry user="test"/>
       </div>
     );
   }
---
+++ app/src/components/PantrySort.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class PantrySort extends Component{
+	render(){
+		return(
+			<div className="pantry-sort">
+				<h2>Sorting Functions here</h2>
+			</div>
+		);
+	}
+}
+
+export default PantrySort;

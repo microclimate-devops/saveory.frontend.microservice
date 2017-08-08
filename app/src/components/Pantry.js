8e5411507a2d250e334cd356097b13b0226689da
root <root@dps-ubuntu-cfcboot.rtp.raleigh.ibm.com>
2017-08-08 11:04:39 -0400

    trying https again

--- app/src/components/Pantry.js
+++ app/src/components/Pantry.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import Https from 'https';
 import PantrySort from "./PantrySort.js";
 import PantryIngredients from "./PantryIngredients.js";
 
@@ -10,9 +11,18 @@ class Pantry extends Component{
 	}
 
 	componentDidMount(){ //retrieve the user's pantry from the backend
-		axios.get("http://microservicetalkingbackend-service:9080/microservicetalkingbackend/pantries?user="/*"http://dps-ubuntu-cfcmaster.rtp.raleigh.ibm.com:8443/kubernetes/api/v1/proxy/namespaces/default/services/microservicetalkingbackend-service:9080/microservicetalkingbackend/pantries?user="*/+this.props.user).then(res => {
+		/*axios.get("http://microservicetalkingbackend-service:9080/microservicetalkingbackend/pantries?user="+this.props.user).then(res => {
 			this.setState({pantry: res.data, error: {error: "none"}});
-		}).catch((err)=> {this.setState({error: err})});
+		}).catch((err)=> {this.setState({error: err})});*/
+		
+		Https.get("https://microservicetalkingbackend-service:9080/microservicetalkingbackend/pantries?user="+this.props.user, (res) => {
+			res.on('data', (d) => {
+				console.log("data: "+JSON.stringify(d));
+				this.setState({pantry: d.pantry});	
+			});
+		}).on('error', (e) => {
+			console.log(this.setState({error: e}));
+		});
 	}
 
 	render(){

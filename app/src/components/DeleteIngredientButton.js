639fe151c43b736e540e68dfde0bee28012a45fb
Daniel Schartner <daniel.schartner@ibm.com>
2017-08-09 15:12:33 -0400

    testing delete ingredient

--- app/src/components/DeleteIngredientButton.js
+++ app/src/components/DeleteIngredientButton.js
@@ -7,8 +7,8 @@ class DeleteIngredientButton extends Component{
 	};
 
   onPress(e){
-    console.log("e from child: " JSON.stringify(e));
-    console.log(JSON.stringify(this.props.deleteIngredientHandler);
+    console.log("e from child: " +JSON.stringify(e));
+    console.log(JSON.stringify(this.props.deleteIngredientHandler));
   }
 
   render(){

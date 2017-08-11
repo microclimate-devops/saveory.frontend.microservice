a738d30eb4e8c005c8ba63608234c19b5ebc2d6e
root <root@dps-ubuntu-cfcboot.rtp.raleigh.ibm.com>
2017-08-11 13:05:54 -0400

    Configuring react router

--- app/src/App.js
+++ app/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Router, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import './App.css';
 //import components
 import Header from './components/Header.js';

f67aaf2c85ee18f63fbc06521e19dd6444838cb8
root <root@dps-ubuntu-cfcboot.rtp.raleigh.ibm.com>
2017-08-11 12:59:59 -0400

    figuring out pantry bug

--- app/src/App.js
+++ app/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Route} from 'react-router-dom';
+import {Router, Route} from 'react-router-dom';
 import './App.css';
 //import components
 import Header from './components/Header.js';

e89f80b8b769a58a2630754d645efa44af26ef3c
root <root@dps-ubuntu-cfcboot.rtp.raleigh.ibm.com>
2017-08-18 15:15:23 -0400

    Setting internal state to store the response from the backend service

--- app/src/components/Recipes.js
+++ app/src/components/Recipes.js
@@ -120,7 +120,7 @@ class Recipes extends Component{
 		}).catch((e) => {
 			this.setState({error: e});
 		});*/
-		Client.search(recipeRequestURL, (response) => {console.log(response)});
+		Client.search(recipeRequestURL, (response) => {this.setState({recipesDB: response})});
 	}
 
 	isQueryMatch(query, target){

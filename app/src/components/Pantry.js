b7c1f9e8bfb52fb79d9b5378cea11389fb80f8d2
Daniel Schartner <daniel.schartner@ibm.com>
2017-09-11 13:18:33 -0400

    The pantry service is now incorporated

--- app/src/components/Pantry.js
+++ app/src/components/Pantry.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import {InlineNotification} from 'carbon-components-react';
+import Client from './Client.js';
 import PantryTable from './PantryTable.js';
 //import axios from 'axios';
 import Https from 'https';
@@ -11,9 +12,10 @@ class Pantry extends Component {
 	constructor(props){
 		super(props);
 		this.state = {
+			pantryServiceURL: "/api/pantry/",
 			pantry: [ { "item" : "apple" , "qty" : 4 , "qtyUnit" : "piece" , "expDate" : "08-04-2017"} , { "item" : "whole milk" , "qty" : 2 , "qtyUnit" : "gallon" , "expDate" : "08-10-2017"}],
 			error: {}, 
-			pantryServiceURL: "https://dps-ubuntu-cfcmaster.rtp.raleigh.ibm.com:8443/kubernetes/api/v1/proxy/namespaces/default/services/microservicetalkingbackend-service:9080/microservicetalkingbackend/", 
+
 			pantryEmptyDescriptor: {
 				item: "Empty",
 				qty: "Empty",
@@ -58,14 +60,15 @@ class Pantry extends Component {
 		};
 		this.deleteIngredient = this.deleteIngredient.bind(this);
 		this.addIngredient = this.addIngredient.bind(this);
+		this.setPantry = this.setPantry.bind(this);
 	}
 
 	static propTypes = {
 		user: PropTypes.string.isRequired
 	};
 
-	retrievePantry(){
-		const pantryRequestURL = this.state.pantryServiceURL+"pantries?user="+this.props.user;
+	retrievePantry_old(){
+		const pantryRequestURL = "https://dps-ubuntu-cfcmaster.rtp.raleigh.ibm.com:8443/kubernetes/api/v1/proxy/namespaces/default/services/microservicetalkingbackend-service:9080/microservicetalkingbackend/pantries?user="+this.props.user;
 		//retrieve the user's pantry from the backend
 		Https.get(pantryRequestURL, (res) => {
 			res.on('data', (d) => {
@@ -88,6 +91,59 @@ class Pantry extends Component {
 		});
 	}
 
+	setNotification(opts){
+		//Set values from passed options, apply defaults if necessary 
+		let {title, subtitle, isGood} = {title: (opts.title || "No Title"), subtitle:(opts.subtitle || "No Message"), isGood:(opts.isGood || false)};
+		this.setState({actionMsgType: isGood ? "success" : "error", actionMsgTitle: title, actionMsgSubtitle: subtitle, showActionMsg: true});
+		
+	}
+
+	setPantry(resp){
+		console.log(resp);
+		//If the response sent back an internal server error code, set an error notification 
+		if(resp.code !== undefined && resp.code === 500){
+			this.setNotification({title: "Error", subtitle:"Internal application error, there was a problem retrieving the user's pantry from the backend", type:"error"});
+		}else{
+			this.setState({pantry: resp.pantry});
+		}
+	}
+
+	handlePantryResponse(resp){
+		//Handle different response codes
+		if(resp.code !== undefined){
+			switch(resp) {
+				case 200:
+					//Success!!
+					this.setNotification({title: "Success", subtitle:resp.msg, isGood:true});
+					//Need to refresh the pantry
+					this.retrievePantry();
+					break;
+				case 500:
+					this.setNotification({title: "Error", subtitle:resp.msg, isGood:false});
+					break;
+					
+					
+			}
+		}else if(resp.pantry != undefined){ //The response is the pantry itself
+			this.setState({pantry: resp.pantry});
+		}else{ //Unknown Condition
+			console.log("The response from the pantry service does not make sense: ");
+			console.log(resp);
+			this.setNotification({title: "Error", subtitle:"Unable to access user's pantry", isGood:false});
+			
+		}
+	}
+
+	testGetPantry(url){
+		//this.setPantry({"id":"59b3152846e0fb000175a6e4","user":"daniel","pantry":[{"item":"apple","qty":10,"qtyUnit":"piece","expDate":"08-04-2017","contains":"false"}]});
+		this.setPantry({code: 500, status:"Error", msg:"Failed"});
+	}
+
+	retrievePantry(){
+		const pantryRequestURL = this.state.pantryServiceURL + this.props.user;
+		Client.request(pantryRequestURL, "GET", (response) => {this.handlePantryResponse(response)});
+	}
+
 	componentDidMount(){
 		this.retrievePantry();
 	}
@@ -106,29 +162,9 @@ class Pantry extends Component {
 	}
 
 	deleteIngredient(ingredient){
-		console.log("PASSED INGREDIENT: "+ingredient.item);
-		//TEST before backend implementation
-		
-		//remove the item from the pantry
-		let userPantry = this.state.pantry;
-		let indexToDelete = this.findIngredientInPantry(ingredient.item);
-		let actionMsgTitle = "Success"
-		let actionMsgSubtitle = "The ingredient was deleted"
-		let actionMsgType = "success";
-		let showActionMsg = true;
-
-		//console.log("pantry before delete at index "+indexToDelete+"("+(indexToDelete===undefined)+"), " + JSON.stringify(userPantry));
-		//Check that index is valid
-		if(indexToDelete !== undefined){
-			userPantry.splice(indexToDelete, 1);
-		}else{
-			actionMsgTitle = "Failure";
-			actionMsgSubtitle = "Failed to delete message";
-			actionMsgType = "error"
-		}
-		//console.log("pantry after delete at index "+indexToDelete+", " + JSON.stringify(userPantry));
-		//Update pantry and message stuff
-		this.setState({pantry: userPantry, actionMsgType: actionMsgType, actionMsgTitle: actionMsgTitle, actionMsgSubtitle: actionMsgSubtitle, showActionMsg: showActionMsg});
+		//send request to delete the ingredient
+		const pantryRequestURL = this.state.pantryServiceURL + this.props.user;
+		Client.request(pantryRequestURL + "/ingredient/" + ingredient.item, "DELETE", (response) => {this.handlePantryResponse(response)});
 	}
 
 	ingredientIsValid(ingredient){
@@ -147,20 +183,9 @@ class Pantry extends Component {
 	}
 
 	addIngredient(ingredient){
-		let userPantry = this.state.pantry;
-		let actionMsgTitle = "Failure"
-		let actionMsgSubtitle = "Could not add ingredient. Please make sure the ingredient is not already in the pantry and fill all fields. "
-		let actionMsgType = "error";
-		let showActionMsg = true;
-		if(this.ingredientIsValid(ingredient)){
-			userPantry.push(ingredient);
-			actionMsgTitle = "Success"
-			actionMsgSubtitle = "Ingredient was added"
-			actionMsgType = "success";
-		}
-
-		//Update pantry and message stuff
-		this.setState({pantry: userPantry, actionMsgType: actionMsgType, actionMsgTitle: actionMsgTitle, actionMsgSubtitle: actionMsgSubtitle, showActionMsg: showActionMsg});
+		//send a request to add the ingredient
+		const pantryRequestURL = this.state.pantryServiceURL + this.props.user;
+		Client.request(pantryRequestURL + "/ingredient", "POST", (response) => {this.handlePantryResponse(response)});
 	}
 
 	showNotification(){
